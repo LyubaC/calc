c3579ab84d350545d16c527da9acd21a398d764e
Lyubov Chepil <lyubac@ukr.net>
2018-08-07 21:20:25 +0000

    Fix

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 const display = document.querySelector('.calc .display');
+var res = false;
 
 document.querySelectorAll('.calc .num, .calc .op')
     .forEach ( btn => btn.addEventListener('click', numOperClick));
     
 function numOperClick(e) {
+    if (res) {
+        res = false;
+        display.value = "";
+    }
     display.value += e.target.innerText
 }    
 
 document.querySelector('.calc .eq')
     .addEventListener('click', function(){
         display.value = eval(display.value);
+        res = true;
 })
+
+document.querySelector('.calc .clear')
+    .addEventListener('click', function(){
+        display.value = "";
+    })
     
 
     
